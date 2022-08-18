1cd1a7e83d5d4fe3e16089c2b8cf8818a726dfb9
Ludwig Schneider <ludwigschneider@uchicago.edu>
2022-08-17 19:40:12 -0500

    add bond compatibility to bond descriptors (#7)
    * add bond compatibility to bond descriptors
    
    * use conda in actions
    
    * use conda pip
    
    * typo

--- tests/test_bond.py
+++ tests/test_bond.py
@@ -5,19 +5,63 @@
 import bigsmiles_gen
 
 
-def test_descriptors():
+def test_descriptors_str():
 
     test_args = [
-        ("[$]", 0, "", "[$|1.0|]"),
+        ("[$0]", 0, "", "[$0|1.0|]"),
         ("[<]", 0, "", "[<|1.0|]"),
         ("[>]", 1, "", "[>|1.0|]"),
         ("[$]", 0, "-", "[$|1.0|]"),
         ("[<]", 0, "#", "[<|1.0|]"),
-        ("[>]", 1, "@=", "[>|1.0|]"),
-        ("[$| 2.|]", 34, "", "[$|2.0|]"),
+        ("[>]", 1, "=", "[>|1.0|]"),
+        ("[$5| 2.|]", 34, "", "[$5|2.0|]"),
         ("[<| 21. 234. 2134. 64. 657.|]", 0, "#", "[<|21.0 234.0 2134.0 64.0 657.0|]"),
     ]
 
     for text, idx, char, ref in test_args:
         bond = bigsmiles_gen.BondDescriptor(text, idx, char)
         assert str(bond) == char + ref
+
+
+def test_descriptors_compatible():
+
+    bondA = bigsmiles_gen.BondDescriptor("[$]", 0, "")
+    bondB = bigsmiles_gen.BondDescriptor("[$]", 0, "")
+
+    assert bondA.is_compatible(bondB)
+    assert bondB.is_compatible(bondA)
+
+    bondA = bigsmiles_gen.BondDescriptor("[$0]", 0, "")
+    bondB = bigsmiles_gen.BondDescriptor("[$0]", 0, "")
+
+    assert bondA.is_compatible(bondB)
+    assert bondB.is_compatible(bondA)
+
+    bondA = bigsmiles_gen.BondDescriptor("[$0]", 0, "")
+    bondB = bigsmiles_gen.BondDescriptor("[$1]", 0, "")
+
+    assert not bondA.is_compatible(bondB)
+    assert not bondB.is_compatible(bondA)
+
+    bondA = bigsmiles_gen.BondDescriptor("[<]", 0, "")
+    bondB = bigsmiles_gen.BondDescriptor("[<]", 0, "")
+
+    assert not bondA.is_compatible(bondB)
+    assert not bondB.is_compatible(bondA)
+
+    bondA = bigsmiles_gen.BondDescriptor("[>]", 0, "")
+    bondB = bigsmiles_gen.BondDescriptor("[>]", 0, "")
+
+    assert not bondA.is_compatible(bondB)
+    assert not bondB.is_compatible(bondA)
+
+    bondA = bigsmiles_gen.BondDescriptor("[<]", 0, "")
+    bondB = bigsmiles_gen.BondDescriptor("[>]", 0, "")
+
+    assert bondA.is_compatible(bondB)
+    assert bondB.is_compatible(bondA)
+
+
+if __name__ == "__main__":
+    test_descriptors_str()
+    test_descriptors_compatible()
