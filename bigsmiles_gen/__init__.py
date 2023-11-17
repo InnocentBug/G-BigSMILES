32d254be3d5b32c86e2369ee89663c9f008546e5
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-11-17 09:29:45 -0600

    Add warning message for non-installed execution of package (#46)

--- bigsmiles_gen/__init__.py
+++ bigsmiles_gen/__init__.py
@@ -8,8 +8,14 @@ bigSMILES extension to generation SMILES string ensembles.
 """
 
 from . import distribution
-from ._version import version as __version__  # noqa: E402, F401
-from ._version import version_tuple
+
+try:
+    from ._version import version as __version__  # noqa: E402, F401
+    from ._version import version_tuple
+except ImportError as exc:
+    raise RuntimeError(
+        "Please make sure to install this module correctly via setuptools with setuptools_scm activated to generate a `_version.py` file."
+    ) from exc
 from .bond import BondDescriptor
 from .core import _GLOBAL_RNG, BigSMILESbase, reaction_graph_to_dot_string
 from .distribution import Distribution, FlorySchulz, Gauss
