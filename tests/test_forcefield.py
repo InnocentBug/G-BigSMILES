fb4626034ae1bb7c320a9513edd41acdb8373766
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-12-12 18:06:01 -0600

    add simple forcefield assignment test

---
+++ tests/test_forcefield.py
@@ -0,0 +1,9 @@
+import bigsmiles_gen
+
+
+def test_forcefield_assignment():
+    smi = "CCC(C){[>][<]CC([>])c1ccccc1[<]}|schulz_zimm(2000, 1800)|{[>][<]CC([>])C(=O)OC[<]}|schulz_zimm(1000, 900)|[H]"
+    mol = bigsmiles_gen.Molecule(smi)
+    mol_gen = mol.generate()
+    ffparam, mol = mol_gen.forcefield_types
+    assert len(ffparam) == mol.GetNumAtoms()
